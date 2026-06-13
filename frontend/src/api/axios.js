427d1e201214ed94bac56de7445285908fe8f48c
varshith1024 <blackpantherpc2@gmail.com>
2026-06-14 01:22:29 +0530

    final

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+console.log("API URL =", process.env.REACT_APP_API_URL);
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
